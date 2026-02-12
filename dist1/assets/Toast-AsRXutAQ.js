c0a43a89e92d094b8587eb2197a3d8a54f6109e0
scarlet0o0 <67679972+scarlet0o0@users.noreply.github.com>
2026-02-12 17:14:51 +0900

    Add files via upload

---
+++ dist1/assets/Toast-AsRXutAQ.js
@@ -0,0 +1,1 @@
+import{a_ as s}from"./index-DORryCv9.js";const a=(o,r={})=>{s.error(o,{position:"top-right",autoClose:3e3,...r})};export{a as s};
---
+++ dist1/assets/screenClientService-DAVww-of.js
@@ -0,0 +1,1 @@
+import{ay as t}from"./index-DORryCv9.js";const n={async getAllScreenClients(){const{data:e}=await t.get("/api/v1/screen-client/all");return e}};export{n as s};
