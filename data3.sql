96287af4a878b1503456a435c000c9c449386301
scarlet0o0 <67679972+scarlet0o0@users.noreply.github.com>
2026-03-06 14:07:11 +0900

    Add files via upload

---
+++ data3.sql
@@ -0,0 +1,50 @@
+-- --------------------------------------------------------
+-- 호스트:                          qst-s.iptime.org
+-- 서버 버전:                        10.3.39-MariaDB-0ubuntu0.20.04.2 - Ubuntu 20.04
+-- 서버 OS:                        debian-linux-gnu
+-- HeidiSQL 버전:                  12.6.0.6765
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+-- 테이블 데이터 paradise.tbl_group_info:~26 rows (대략적) 내보내기
+INSERT INTO `tbl_group_info` (`groupId`, `groupName`, `groupType`) VALUES
+	('101', 'JUMBO', '0'),
+	('102', 'DFDC GRAND', '0'),
+	('103', 'COIN COMBO', '0'),
+	('104', 'JJBX', '0'),
+	('105', 'SUPER HOT FLAMING POT', '0'),
+	('106', 'HUFF EVEN MORE PUFF', '0'),
+	('107', 'GOOD FORTURN', '0'),
+	('108', 'DFDC', '0'),
+	('109', 'SSFT2_GRAND', '0'),
+	('110', 'SSFT2_MAJOR', '0'),
+	('112', 'M_GRAND', '0'),
+	('113', 'M_MAJOR', '0'),
+	('114', 'M_MINOR', '0'),
+	('115', 'M_MEGA2', '0'),
+	('130', 'R_Evil_Grand', '0'),
+	('131', 'HOD_Grand', '0'),
+	('141', 'THREE EYED GOD MAJOR', '0'),
+	('143', 'EXTREME WILD LANTERMS GRAND', '0'),
+	('4007', 'HL DRAGON', '0'),
+	('4008', 'HL TIGER', '0'),
+	('4009', 'HL PHOENIX', '0'),
+	('6004', 'ETG_PROMO_LEVEL1', '0'),
+	('6006', 'ETG_PROMP_LEVEL 2', '0'),
+	('901', 'Blackjack21+3', '0'),
+	('99993', 'TEST L1', '0'),
+	('99994', 'TEST L2', '0');
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
